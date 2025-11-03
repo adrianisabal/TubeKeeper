ca083eafdbd2b7e956353fbc9b8a576d39de833c
adrianisabal <adrian.isabal@opendeusto.es>
2025-11-03 16:31:55 +0100

    basic SearchView constructor

--- src/gui/Sidebar.java
+++ src/gui/Sidebar.java
@@ -2,21 +2,25 @@ package gui;
 
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
+import javax.swing.border.EmptyBorder;
 
 public class Sidebar extends JPanel {
 	
 	public Sidebar() {
 		
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		setPreferredSize(new Dimension(750, 80));
+		setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+
+		setMaximumSize(new Dimension(750, 80));
 		
 		add(new JButton("Expand"));		
-		add(new JSeparator());
+		//add(new JSeparator());
 		
 		add(new JButton("Playlists Menu"));
 		add(new JButton("History"));
