7a265b586fed9b2bce4210255cc5a8b689966ed3
fragi0 <ssejico@gmail.com>
2025-11-06 11:37:23 +0100

    Sidebar buttons function added + MainFrame CardLayout set

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,46 +1,63 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPanel;
 
 import gui.tools.Sidebar;
 import gui.views.DownloadsView;
 import gui.views.JVideoView;
 import gui.views.PlaylistView;
 import gui.views.SearchView;
+import gui.views.View;
 
 public class MainFrame extends JFrame {
-	
-	public MainFrame() {
-	setSize(1200, 800);
-	setTitle("TubeKeeper");
 
-	setLocationRelativeTo(null);
-	
-	((JComponent) getContentPane()).setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-	
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-	Sidebar sidebar = new Sidebar();
-	add(sidebar, BorderLayout.WEST);
-	
-	SearchView search = new SearchView();
-	//add(search, BorderLayout.CENTER);
-	
-	DownloadsView downloads = new DownloadsView();
-	//add(downloads, BorderLayout.CENTER);
-	
-	JVideoView videoView = new JVideoView();
-	//add(videoView, BorderLayout.CENTER);
-	
-	PlaylistView playlistView = new PlaylistView();
-	add(playlistView, BorderLayout.CENTER);
-	
-	setVisible(true);
-	}
-	
+    public static final String VIEW_SEARCH     = "SEARCH";
+    public static final String VIEW_DOWNLOADS  = "DOWNLOADS";
+    public static final String VIEW_PLAYLISTS  = "PLAYLISTS";
+    public static final String VIEW_VIDEO      = "VIDEO";
+
+    private CardLayout cardLayout = new CardLayout();
+    private JPanel contentPanel = new JPanel(cardLayout);
+    private Map<String, View> views = new HashMap<>();
+
+    public MainFrame() {
+
+        setSize(1200, 800);
+        setTitle("TubeKeeper");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ((JComponent) getContentPane()).setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        
+        Sidebar sidebar = new Sidebar(this);
+        add(sidebar, BorderLayout.WEST);
+
+        add(contentPanel, BorderLayout.CENTER);
+
+        views.put(VIEW_SEARCH, new SearchView());
+        views.put(VIEW_DOWNLOADS, new DownloadsView());
+        views.put(VIEW_VIDEO, new JVideoView());
+        views.put(VIEW_PLAYLISTS, new PlaylistView());
+
+        contentPanel.add(views.get(VIEW_SEARCH), VIEW_SEARCH);
+        contentPanel.add(views.get(VIEW_DOWNLOADS), VIEW_DOWNLOADS);
+        contentPanel.add(views.get(VIEW_VIDEO), VIEW_VIDEO);
+        contentPanel.add(views.get(VIEW_PLAYLISTS), VIEW_PLAYLISTS);
+
+        showScreen(VIEW_SEARCH);
+        setVisible(true);
+    }
+
+    public void showScreen(String id) {
+        cardLayout.show(contentPanel, id);
+    }
 }
