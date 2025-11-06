5e3e37d8545d2499182e84a4d7afa80e39e50fa5
fragi0 <ssejico@gmail.com>
2025-11-06 17:49:53 +0100

    Settings show fix + MainFrame almost completed

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -16,6 +16,7 @@ import gui.views.JVideoView;
 import gui.views.PlaylistMenuView;
 import gui.views.PlaylistView;
 import gui.views.SearchView;
+import gui.views.SettingsView;
 import gui.views.View;
 
 public class MainFrame extends JFrame {
@@ -49,15 +50,15 @@ public class MainFrame extends JFrame {
         views.put(VIEW_SEARCH, new SearchView());
         views.put(VIEW_DOWNLOADS, new DownloadsView());
         views.put(VIEW_VIDEO, new JVideoView());
-        views.put(VIEW_PLAYLISTS, new PlaylistView());
-//        views.put(VIEW_SETTINGS, new SettingsView()); //TODO Crear constructores y la view de settings bien
+        views.put(VIEW_PLAYLISTS, new PlaylistMenuView());
+        views.put(VIEW_SETTINGS, new SettingsView()); //TODO Crear constructores y la view de settings bien
 
 
         contentPanel.add(views.get(VIEW_SEARCH), VIEW_SEARCH);
         contentPanel.add(views.get(VIEW_DOWNLOADS), VIEW_DOWNLOADS);
         contentPanel.add(views.get(VIEW_VIDEO), VIEW_VIDEO);
         contentPanel.add(views.get(VIEW_PLAYLISTS), VIEW_PLAYLISTS);
-//        contentPanel.add(views.get(VIEW_SETTINGS), VIEW_SETTINGS);
+        contentPanel.add(views.get(VIEW_SETTINGS), VIEW_SETTINGS);
 
         showScreen(VIEW_SEARCH);
         setVisible(true);
--- src/gui/views/SettingsView.java
+++ src/gui/views/SettingsView.java
@@ -1,10 +1,42 @@
 package gui.views;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import gui.views.View.ViewType;
+
 public class SettingsView extends View{
 
-	public SettingsView(ViewType type, String title) {
-		super(type, title);
-		// TODO Auto-generated constructor stub, hay que hacer esto bien
+	public SettingsView() {
+		super(ViewType.MAIN_VIEW, "Settings");
+		
+		
+		JPanel gridBag = new JPanel(new GridBagLayout());
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.insets = new Insets(5,5,5,5);
+		
+		//TITLE
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;   // Cuántas columnas de ancho ocupa
+		gbc.weightx = 1.0;   // TODO Pilla todo lo que te quede horizontalmente //TODO probar a poner esto en 0
+		gbc.fill = GridBagConstraints.HORIZONTAL; // Se estira horizontalmente
+		gbc.anchor = GridBagConstraints.CENTER;   // Centrado en su celda
+		
+		JLabel title = new JLabel("Prueba");
+		add(title, gbc);
+		
+		
+//		add(Panel, BorderLayout.CENTER);
 	}
+	
+	
 
 }
